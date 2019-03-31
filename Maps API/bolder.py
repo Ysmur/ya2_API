1600db06a422d21682e452cb117d168ff290899d
Elena M <gym625.ysmur@yandexlyceum.ru>
2019-03-31 14:11:31 +0300

    Вспомогательная функция bolder, которая вычисляет дельту на основе размеров объекта.

---
+++ Maps API/bolder.py
@@ -0,0 +1,24 @@
+import requests
+from geocode import geocode
+
+def bolder(address):
+    """ координаты объекта по его адресу
+    обращаемся к функции geocode из файла geocode.py"""
+    toponym = geocode(address)
+    if toponym:
+        # Рамка вокруг объекта:
+        envelope = toponym["boundedBy"]["Envelope"]
+
+        # левая, нижняя, правая и верхняя границы из координат углов:
+        l, b = envelope["lowerCorner"].split(" ")
+        r, t = envelope["upperCorner"].split(" ")
+
+        # Вычисляем полуразмеры по вертикали и горизонтали
+        dx = abs(float(l) - float(r)) / 2.0
+        dy = abs(float(t) - float(b)) / 2.0
+
+        return max(dx, dy)
+    return None
+
+
+# print(bolder('Москва, ул. Ак. Королева, 12'))
